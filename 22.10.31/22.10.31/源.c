c5838de6696d862eeecf022a102c1efd3bf1a435
D-morpheus <1978894853@qq.com>
2022-11-01 12:01:56 +0800

    2022.11.1 12：01

---
+++ 22.10.31/22.10.31/源.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//
+//	printf("%p\n", arr);
+//	printf("%p\n", &arr[0]);
+//	return 0;
+//
+//}
+
+
+struct people
+{
+	char name[20];
+	int age;
+
+};
+
+int main()
+{
+
+
+	return 0;
+}
