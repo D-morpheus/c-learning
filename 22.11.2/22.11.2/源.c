7409aa1a0f2d38051c3b9b827e36d710a1841aef
D-morpheus <1978894853@qq.com>
2022-11-03 13:09:29 +0800

    2022.11.3 13：09

---
+++ 22.11.2/22.11.2/源.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	//char* str3 = "hello bit";
+	//char* str4 = "hello bit";
+
+	//int i = (str3 == str4);
+	//printf("%d\n", i);
+
+	//return 0;
+
+	double* arr[5];
+
+	double* (*pd)[5] = &arr;
+
+	int (*p)[5]
+}
