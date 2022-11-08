6628d0dc5db995d74ea344ac3247a4bfffa33892
D-morpheus <1978894853@qq.com>
2022-11-08 23:08:13 +0800

    2022.11.8 23：08

---
+++ 22.11.8/22.11.8/源.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+//
+//int main()
+//{
+//    long long i = 1;
+//    int n = 0;
+//    scanf("%d", &n);
+//    int count = 1;
+//
+//    while (1)
+//    {
+//        if (i % n == 0)
+//        {
+//            break;
+//        }
+//        i = i * 10 + 1;
+//        count++;
+//    }
+//    printf("%d", count);
+//    return 0;
+//
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int a[100] = { 0 };
+//	int b[100] = { 0 };
+//
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &b[i]);
+//	}
+//
+//	for (i = 0; i < n; i++)
+//	{
+//		int j = b[i];
+//		int tmp = a[i];
+//		a[i] = a[j];
+//		a[j] = tmp;
+//	}
+//
+//	for (i = 0; i < n; i++)
+//	{
+//		if (i == n - 1)
+//		{
+//			printf("%d\n", a[i]);
+//			break;
+//		}
+//		printf("%d ", a[i]);
+//	}
+//
+//
+//	return 0;
+//}
+
+int main()
+{
+	
+	int i = 0;
+	int a[100] = { 0 };
+	int b[100] = { 0 };
+
+	for (i = 0; ; i++)
+	{
+		scanf("%d", &a[i]);
+		if(i)
+	}
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &b[i]);
+	}
+}
